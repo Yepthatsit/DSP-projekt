35ee74110be97548ace6c53f806d93e5a13dbea6
KarolGrzywa <karolgrzywa@herp.pl>
2025-07-27 15:03:12 +0200

    audoio reading

--- AppFunction.py
+++ AppFunction.py
@@ -1,24 +1,33 @@
 import asyncio
-from tkinter import filedialog
+from PyQt6.QtWidgets import QFileDialog
 import audioread
+import matplotlib.pyplot as plt
+from Interface import Ui_Wizualizator_audio
 class AppFunctionService:
-    def __init__(self, app):
+    def __init__(self):
         self.SelectedFilePath = ""
         self.SampleRate = 0
         self.FileBytes = []
         self.frameSize = 1024
         
-    async def readFile(self):
-        filepath = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3 *.flac")])
+    def readFile(self,ui:Ui_Wizualizator_audio = None):
+        print("Reading file...")
+        filepath, _ = QFileDialog.getOpenFileName(
+            None,
+            "Wybierz plik audio",
+            "",
+            "Pliki audio (*.wav *.mp3 *.flac)"
+        )
         if filepath:
             self.SelectedFilePath = filepath
             self.FileBytes = []
             self.SampleRate = 0
             
-            async with audioread.audio_open(filepath) as audio_file:
+            with audioread.audio_open(filepath) as audio_file:
                 self.SampleRate = audio_file.samplerate
                 self.FileBytes = audio_file.read_data()
-            
+            if(ui):
+                ui.textBrowser.setText(f"Plik: {self.SelectedFilePath.split('/')[-1]}\n")
             print(f"File {self.SelectedFilePath} loaded with sample rate {self.SampleRate} Hz.")
         else:
             print("No file selected.")
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from Interface import Ui_Wizualizator_audio
 from PySide6.QtWidgets import (QApplication, QMainWindow)
+from AppFunction import AppFunctionService
 import sys
-import AppFunction
 
 
-
-
-if __name__ == "__main__":    
+if __name__ == "__main__":
+    appService = AppFunctionService()
+        
     app = QApplication(sys.argv)
     Wizualizator_audio = QMainWindow()
     ui = Ui_Wizualizator_audio()
     ui.setupUi(Wizualizator_audio)
-    ui.pushButton.clicked.connect(lambda: print("Button clicked!"))
+    ui.openFileBTN.clicked.connect(lambda : appService.readFile(ui))
     Wizualizator_audio.show()
     sys.exit(app.exec())
